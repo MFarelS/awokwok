5d95cfdc8167e84fb7d71346e83ece614ce6214c
rashidsiregar28 <74960869+rashidsiregar28@users.noreply.github.com>
2020-12-15 00:15:51 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,678 @@
+function help() {
+    return `
+   ✪═⟪ NEKO BOT MENU ⟫═✪
+ 
+ *CREATOR= RASHID*
+ *TOTAL COMMAND 《128》*
+
+╔══✪〘 GROUP 〙✪══
+║
+╠➥ *❤️snk*
+╠➥ *❤️groupinfo*
+╠➥ *❤️add 62858xxxxx*
+╠➥ *❤️kick @tagmember*
+╠➥ *❤️promote @tagmember*
+╠➥ *❤️demote @tagadmin*
+╠➥ *❤️tagall*
+╠➥ *❤️adminList*
+╠➥ *❤️ownerGroup*
+╠➥ *❤️leave*
+╠➥ *❤️delete [replyChatBot]*
+╠➥ *❤️kickAll*
+╠➥ *❤️group [open|close]*
+╠➥ *❤️NSFW [enable|disable]*
+╠➥ *❤️left [enable|disable]*
+╠➥ *❤️welcome [enable|disable]*
+╠➥ *❤️simi [enable|disable]*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 ADMIN NEKO 〙✪══
+║
+╠➥ *❤️mute*
+╠➥ *❤️unmute*
+╠➥ *❤️ban @tagmember*
+╠➥ *❤️unban @tagmember*
+╠➥ *❤️spamcall [81273xxxx]*
+╠➥ *❤️kickall*
+╠➥ *❤️oleave*
+╠➥ *❤️opromote*
+╠➥ *❤️odemote*
+╠➥ *❤️odelete*
+╠➥ *❤️oadd 62813xxxxx*
+╠➥ *❤️okickall*
+╠➥ *❤️otagall*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 KERANG 〙✪══
+║
+╠➥ *❤️apakah [optional]*
+╠➥ *❤️rate [optional]*
+╠➥ *❤️bisakah [optional]*
+╠➥ *❤️kapankah [optional]*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 DOWNLOADER 〙✪══
+║
+╠➥ *❤️ytmp3 [linkYt]*
+╠➥ *❤️ytmp4 [linkYt]*
+╠➥ *❤️ig [linkIg]*
+╠➥ *❤️fb [linkFb]*
+╠➥ *❤️twitter [linkTwitter]*
+╠➥ *❤️smule [linkSmule]*
+╠➥ *❤️tiktok [linkTiktok]*
+╠➥ *❤️starmaker [linkStarmaker]*
+╠➥ *❤️joox [lagu]*
+╠➥ *❤️play [lagu]*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 MEDIA AND SEARCH 〙✪══
+║
+╠➥ *❤️caklontong*
+╠➥ *❤️family100*
+╠➥ *❤️koin*
+╠➥ *❤️tebakgambar*
+╠➥ *❤️heroml [nama hero]*
+╠➥ *❤️covid [negara]*
+╠➥ *❤️nulis [teks]*
+╠➥ *❤️sandwriting [teks]*
+╠➥ *❤️quotemaker [|teks|author|theme]*
+╠➥ *❤️jadwalTv [channel]*
+╠➥ *❤️cuaca [tempat]*
+╠➥ *❤️resep [optional]*
+╠➥ *❤️maps [query]*
+╠➥ *❤️tts [kode bhs] [teks]*
+╠➥ *❤️kbbi [query]*
+╠➥ *❤️math [query]*
+╠➥ *❤️wiki [query]*
+╠➥ *❤️google [query]*
+╠➥ *❤️pinterest [query]*
+╠➥ *❤️googleimg [query]*
+╠➥ *❤️translate [bahasa] [teks]*
+╠➥ *❤️brainly [pertanyaan] [.jumlah]*
+╠➥ *❤️lirik [optional]*
+╠➥ *❤️chord [optional]*
+╠➥ *❤️qrcode [optional]*
+╠➥ *❤️maps [optional]*
+╠➥ ~*❤️kpop [query]*~
+╠➥ *❤️textmaker [teks1|teks2]*
+╠➥ *❤️checkip [ipaddress]*
+╠➥ ~*❤️ssweb [linkWeb]*~
+╠➥ *❤️shorturl [linkWeb]*
+╠➥ *❤️profile*
+╠➥ *❤️fakta*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 PRIMBON MENU 〙✪══
+║
+╠➥ *❤️ramalpasangan [kamu|pasangan]*
+╠➥ *❤️zodiak [zodiak kamu]*
+╠➥ *❤️artinama [nama]*
+╠➥ *❤️artimimpi [mimpi]*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 STALK MENU 〙✪══
+║
+╠➥ *❤️igstalk [@username]*
+╠➥ *❤️tiktokstalk [@username]*
+╠➥ *❤️smulestalk [@username]*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 PRAY 〙✪══
+║
+╠➥ *❤️quran [urutan surah]*
+╠➥ *❤️infosurah [nama surah]*
+╠➥ *❤️tafsir [nama surah] [ayat]*
+╠➥ *❤️jadwalsholat [daerah]*
+╠➥ *❤️listsurah*
+╠➥ *❤️listdaerah*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 ANIME 〙✪══
+║
+╠➥ *❤️loli*
+╠➥ *❤️shota*
+╠➥ *❤️waifu*
+╠➥ *❤️husbu*
+╠➥ *❤️NekoNime*
+╠➥ *❤️randomAnime*
+╠➥ *❤️quotesnime*
+╠➥ *❤️wait*
+╠➥ *❤️malanime [optional]*
+╠➥ *❤️malcharacter [optional]*
+╠➥ *❤️kusonime [optional]*
+╠➥ *❤️otakudesu [optional]*
+╠➥ *❤️dewabatch [optional]*
+╠➥ *❤️komiku [optional]*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 NSFW 〙✪══
+║
+╠➥ *❤️randomhentai*
+╠➥ *❤️randomnsfwneko*
+╠➥ *❤️randomtrapnime*
+╠➥ *❤️nhentai [kode]*
+╠➥ *❤️nhder [kode]*
+╠➥ *❤️xnxx [linkXnxx]*
+╠➥ *❤️randomblowjob*
+╠➥ ~*❤️nekopoi*~
+╠➥ ~*❤️cersex*~
+║
+╚═〘 NEKO BOT 〙
+
+
+
+╔══✪〘 OWNER 〙✪══
+║
+╠➥ *❤️block 62858xxxxx*
+╠➥ *❤️unblock 62858xxxxx*
+╠➥ *❤️addadmin @tagmember*
+╠➥ *❤️deladmin @tagmember*
+╠➥ *❤️restart*
+╠➥ *❤️ekickall*
+╠➥ *❤️banchat*
+╠➥ *❤️unbanchat*
+╠➥ *❤️eval [kode JavaScript]*
+╠➥ *❤️bc [text]*
+╠➥ *❤️adminlist*
+╠➥ *❤️ownergroup*
+╠➥ *❤️getses*
+╠➥ *❤️leaveall*
+╠➥ *❤️spamcall 628xxxx*
+║
+╚═〘 NEKO BOT 〙
+
+
+╔══✪〘 FUN MENU 〙✪══
+║
+╠➥ *❤️bahasa*
+╠➥ *❤️sticker*
+╠➥ *❤️stickergif*
+╠➥ *❤️sfire*
+╠➥ *❤️slightning*
+╠➥ *❤️ttp [teks]*
+╠➥ *❤️toimg*
+╠➥ *❤️neko*
+╠➥ *❤️pokemon*
+╠➥ *❤️inu*
+╠➥ *❤️infoGempa*
+╠➥ *❤️quotes*
+╠➥ *❤️ptl*
+╠➥ *❤️dadu*
+╠➥ *❤️koin*
+╠➥ *❤️quoterandom*
+╠➥ *❤️motivasi*
+╠➥ *❤️howgay*
+╠➥ *❤️katabijak*
+╠➥ *❤️pantun*
+║
+╚═〘 NEKO BOT 〙
+
+
+╭──✪═⟪ DONASI ⟫═✪
+├≽ ❤️donate
+╰──✪══⟪ END ⟫══✪
+
+─≽ kirim perintah ❤️readme untuk mengetahui fungsi dan penggunaan perintah diatas, WAJIB DIBACA!!
+
+
+pengen masukin bot ke gc? pc owner bot,kontaknya command ❤️owner  😳 
+`
+}
+exports.help = help()
+function ownercmd() {
+    return `
+╔══✪〘 OWNER 〙✪══
+║
+╠➥ *❤️block 62858xxxxx*
+╠➥ *❤️unblock 62858xxxxx*
+╠➥ *❤️addadmin @tagmember*
+╠➥ *❤️deladmin @tagmember*
+╠➥ *❤️restart*
+╠➥ *❤️ekickall*
+╠➥ *❤️banchat*
+╠➥ *❤️unbanchat*
+╠➥ *❤️eval [kode JavaScript]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.ownercmd = ownercmd()
+function admincmd() {
+    return `
+╔══✪〘 ADMIN 〙✪══
+║
+╠➥ *❤️mute*
+╠➥ *❤️unmute*
+╠➥ *❤️ban @tagmember*
+╠➥ *❤️unban @tagmember*
+╠➥ *❤️spamcall [81273xxxx]*
+╠➥ *❤️kickall*
+╠➥ *❤️oleave*
+╠➥ *❤️opromote*
+╠➥ *❤️odemote*
+╠➥ *❤️odelete*
+╠➥ *❤️oadd 62813xxxxx*
+╠➥ *❤️okickall*
+╠➥ *❤️otagall*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.admincmd = admincmd()
+function nsfwcmd() {
+    return `
+╔══✪〘 NSFW 〙✪══
+║
+╠➥ *❤️randomhentai*
+╠➥ *❤️randomnsfwneko*
+╠➥ *❤️randomtrapnime*
+╠➥ *❤️nhentai [kode]*
+╠➥ *❤️nhder [kode]*
+╠➥ *❤️xnxx [linkXnxx]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.nsfwcmd = nsfwcmd()
+function praycmd() {
+    return `
+╔══✪〘 PRAY 〙✪══
+║
+╠➥ *❤️quran [urutan surah]*
+╠➥ *❤️infosurah [nama surah]*
+╠➥ *❤️tafsir [nama surah] [ayat]*
+╠➥ *❤️jadwalsholat [daerah]*
+╠➥ *❤️listsurah*
+╠➥ *❤️listdaerah*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.praycmd = praycmd()
+function kerangcmd() {
+    return `
+╔══✪〘 KERANG 〙✪══
+║
+╠➥ *❤️apakah [optional]*
+╠➥ *❤️rate* [optional]*
+╠➥ *❤️bisakah* [optional]*
+╠➥ *❤️kapankah* [optional]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.kerangcmd = kerangcmd()
+function mediacmd() {
+    return `
+╔══✪〘 MEDIA 〙✪══
+║
+╠➥ *❤️ramalpasangan [kamu|pasangan]*
+╠➥ *❤️zodiak* [zodiak kamu]
+╠➥ *❤️artinama [nama]*
+╠➥ *❤️caklontong*
+╠➥ *❤️family100*
+╠➥ *❤️tebakgambar*
+╠➥ *❤️artinama [nama]*
+╠➥ *❤️artimimpi [mimpi]*
+╠➥ *❤️heroml [nama hero]*
+╠➥ *❤️covid [negara]*
+╠➥ *❤️nulis [teks]*
+╠➥ *❤️sandwriting [teks]*
+╠➥ *❤️quotemaker [|teks|author|theme]*
+╠➥ *❤️jadwalTv [channel]*
+╠➥ *❤️cuaca [tempat]*
+╠➥ *❤️resepmasakan [optional]*
+╠➥ *❤️tts [kode bhs] [teks]*
+╠➥ *❤️igstalk [@username]*
+╠➥ *❤️tiktokstalk [@username]*
+╠➥ *❤️smulestalk [@username]*
+╠➥ *❤️kbbi [query]*
+╠➥ *❤️wiki [query]*
+╠➥ *❤️shopee [query]*
+╠➥ *❤️google [query]*
+╠➥ *❤️pinterest [query]*
+╠➥ *❤️googleimage [query]*
+╠➥ *❤️translate [bahasa] [teks]*
+╠➥ *❤️brainly [pertanyaan] [.jumlah]*
+╠➥ *❤️lirik [optional]*
+╠➥ *❤️chord [optional]*
+╠➥ *❤️qrcode [optional]*
+╠➥ *❤️maps [optional]*
+╠➥ *❤️textmaker [teks1|teks2]*
+╠➥ *❤️checkip [ipaddress]*
+╠➥ ~*❤️ssweb [linkWeb]*~
+╠➥ *❤️shorturl [linkWeb]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.mediacmd = mediacmd()
+function animecmd() {
+    return `
+╔══✪〘 ANIME 〙✪══
+║
+╠➥ *❤️loli*
+╠➥ *❤️shota*
+╠➥ *❤️waifu*
+╠➥ *❤️husbu*
+╠➥ *❤️randomNekoNime*
+╠➥ *❤️randomTrapNime*
+╠➥ *❤️randomAnime*
+╠➥ *❤️quotesnime*
+╠➥ *❤️wait*
+╠➥ *❤️koin*
+╠➥ *❤️malanime [optional]*
+╠➥ *❤️malcharacter [optional]*
+╠➥ *❤️kusonime [optional]*
+╠➥ *❤️otakudesu [optional]*
+╠➥ *❤️dewabatch [optional]*
+╠➥ *❤️komiku [optional]*
+╠➥ *❤️animesearch [query]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.animecmd = animecmd()
+function othercmd() {
+    return `
+╔══✪〘 OTHER 〙✪══
+║
+╠➥ *❤️bahasa*
+╠➥ *❤️sticker*
+╠➥ *❤️stickergif*
+╠➥ *❤️sfire*
+╠➥ *❤️slightning*
+╠➥ *❤️ttp [teks]*
+╠➥ *❤️stickertoimg*
+╠➥ *❤️neko*
+╠➥ *❤️pokemon*
+╠➥ *❤️inu*
+╠➥ *❤️infoGempa*
+╠➥ *❤️quotes*
+╠➥ *❤️ptl*
+╠➥ *❤️dadu*
+╠➥ *❤️koin*
+╠➥ *❤️quoterandom*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.othercmd = othercmd()
+function downloadcmd() {
+    return `
+╔══✪〘 DOWNLOADER 〙✪══
+║
+╠➥ *❤️ytmp3 [linkYt]*
+╠➥ *❤️ytmp4 [linkYt]*
+╠➥ *❤️ig [linkIg]*
+╠➥ *❤️fb [linkFb]*
+╠➥ *❤️twitter [linkTwitter]*
+╠➥ *❤️smule [linkSmule]*
+╠➥ *❤️tiktok [linkTiktok]*
+╠➥ *❤️starmaker [linkStarmaker]*
+╠➥ *❤️joox [lagu]*
+╠➥ *❤️play [lagu]*
+╠➥ *❤️music [lagu]*
+╠➥ *❤️getmusic [IdDownload]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.downloadcmd = downloadcmd()
+function groupcmd() {
+    return `
+╔══✪〘 GROUP 〙✪══
+║
+╠➥ *❤️snk*
+╠➥ *❤️groupinfo*
+╠➥ *❤️add 62858xxxxx*
+╠➥ *❤️kick @tagmember*
+╠➥ *❤️promote @tagmember*
+╠➥ *❤️demote @tagadmin*
+╠➥ *❤️tagall*
+╠➥ *❤️adminList*
+╠➥ *❤️ownerGroup*
+╠➥ *❤️leave*
+╠➥ *❤️delete [replyChatBot]*
+╠➥ *❤️kickAll*
+╠➥ *❤️group [open|close]*
+╠➥ *❤️NSFW [enable|disable]*
+╠➥ *❤️left [enable|disable]*
+╠➥ *❤️welcome [enable|disable]*
+╠➥ *❤️simi [enable|disable]*
+║
+╚═〘 NEKO BOT 〙`
+}
+exports.groupcmd = groupcmd()
+function readme() {
+    return `
+            *「 STALK 」*
+
+*[@username]* Diisi dengan Username yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️tiktokstalk @tobz2k19*
+
+*[@username]* Diisi dengan Username yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️igstalk @tobz2k19*
+
+*[@username]* Diisi dengan Username yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️smulestalk @tobz2k19*
+
+            *「 YT SEARCH 」*
+
+*[video]* Diisi dengan Judul Video yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️video Erpan1140*
+Jika ingin mendownload video harap ketik ❤️getvideo [IdDownload] atau ❤️getvideo [urutan]
+
+*[lagu]* Diisi dengan Judul Lagu yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️music Alan Walker Alone*
+Jika ingin mendownload lagu harap ketik ❤️getmusic [IdDownload] atau ❤️getmusic [urutan]
+
+*[IdDownload] atau [urutan]* Diisi dengan IdDownload yang valid tanpa tanda “[” dan “]”
+Contoh : *Jika tidak reply pesan : ❤️getmusic Iv32bS1*
+Contoh : *Jika reply pesan : ❤️getmusic 1*
+Contoh : *Jika tidak reply pesan : ❤️getvideo Iv32bS1*
+Contoh : *Jika reply pesan : ❤️getvideo 1*
+
+            *「 DOWNLOADER 」*
+
+*[linkStarmaker]* Diisi dengan link Starmaker yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️starmaker https://m.starmakerstudios.com/d/playrecording?app=sm&from_user_id=3096224747920316&is_convert=true&recordingId=10696049124506354&share_type=copyLink*
+
+*[linkTwitter]* Diisi dengan link YouTube yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️twitter https://twitter.com/PassengersMovie/status/821025484150423557*
+
+*[linkXnxx]* Diisi dengan link Xnxx yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️xnxx http://www.xnxx.com/loli/stev-gay*
+
+*[linkNekopoi]* Diisi dengan link Nekopoi yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️nekopoi https://nekopoi.care/tsunpuri-episode-1-subtitle-indonesia/*
+
+*[linkYt]* Diisi dengan link YouTube yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️ytmp3 https://youtu.be/Bskehapzke8*
+
+*[linkYt]* Diisi dengan link YouTube yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️ytmp4 https://youtu.be/Bskehapzke8*
+
+*[linkTiktok]* Diisi dengan link Tiktok yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️tiktok https://vt.tiktok.com/yqyjPX/*
+
+*[linkSmule]* Diisi dengan link Smule yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️smule https://www.smule.com/p/767512225_3062360163*
+
+*[linkIg]* Diisi dengan link Instagram yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️ig https://www.instagram.com/p/CFqRZTlluAi/?igshid=1gtxkbdqhnbbe*
+
+*[linkFb]* Diisi dengan link Facebook yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️fb https://www.facebook.com/EpochTimesTrending/videos/310155606660409*
+
+*[linkTiktok]* Diisi dengan link facebookt Tiktok yang valid tanpa tanda “[” dan “]”
+Contoh : *❤️tiktok https://vt.tiktok.com/yqyjPX/*
+
+            *「 OTHER 」*
+
+*[daerah]* Diisi dengan daerah yang valid, tanpa tanda “[” dan “]”
+Contoh : *❤️jadwalShalat Tangerang*
+
+*[channel]* Diisi dengan channel televisi yang valid, tanpa tanda “[” dan “]”
+Contoh : *❤️jadwalTv Indosiar*
+
+*[query]* Diisi dengan query/pencarian yang valid, tanpa tanda “[” dan “]“
+Contoh : *❤️googlesearch siapa itu neko*
+
+*[tempat]* Diisi dengan tempat/lokasi yang valid, tanpa tanda “[” dan “]“
+Contoh : *❤️cuaca tangerang*
+
+*[kode bhs]* Diisi dengan kode bahasa, contoh *id*, *en*, dll. Dan *[teks]* Diisi dengan teks yang ingin di jadikan voice, Masih sama seperti di atas tanpa tanda “[” dan “]”
+Contoh : *❤️tts id Test*
+Note : Max 250 huruf
+
+*[|teks|author|theme]* Diisi dengan teks, author, dan theme, tanpa tanda “[” dan “]”
+Contoh : *❤️quotemaker |Odading|Mang Oleh|Shark*
+
+*[optional]* Diisi dengan teks|title lirik lagu, tanpa tanda “[” dan “]”.
+Contoh : *❤️lirik aku bukan boneka*
+
+*[ipaddress]* Diisi dengan Ip Address yang valid, tanpa tanda “[” dan “]”.
+Contoh : *❤️checkip 182.0.144.145*`
+}
+exports.readme = readme()
+function info() {
+    return `
+╔══✪〘 INFORMATION 〙✪══
+║
+╠➥ *BOT TYPE : NODEJS V12*
+╠➥ *NAME : NEKO BOT*
+╠➥ *VERSION : 1.0*
+║
+╚═〘 NEKO BOT 〙
+`
+}
+
+exports.info = info()
+function snk() {
+    return `Syarat dan Ketentuan Bot *NEKO*
+1. Teks dan nama pengguna WhatsApp anda akan kami simpan di dalam server selama bot aktif
+2. Data anda akan di hapus ketika bot Offline
+3. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
+4. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
+5. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
+6. Apapun yang anda perintah pada bot ini, KAMI TIDAK AKAN BERTANGGUNG JAWAB!
+
+Thanks !`
+}
+exports.snk = snk()
+function sewa() {
+    return `
+╔══✪〘 IKLAN 〙✪══
+║
+╠➥ *DAFTAR SEWA BOT :*
+╠➥ *SEWA : 10K/GRUP (BULAN)*
+╠➥ *PEMBAYARAN BISA MELALUI :*
+╠➥ *OVO, PAYPAL, DANA, PULSA +5K*
+╠═══════════════════════════
+╠➥ *KEUNTUNGAN SEWA BOT :*
+╠➥ *1. BISA MENJADI ADMIN neko*
+╠➥ *2. BISA MENDAPATKAN COMMAND ADMIN*
+╠➥ *JIKA MINAT IKLAN DIATAS*
+╠➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
+╠➥ *wa.me/628127668234*
+║
+╚═〘 NEKO BOT 〙
+`
+}
+exports.sewa = sewa()
+function sumbang() {
+    return `
+╔══✪〘 DONATE 〙✪══
+║
+╠➥ *DONASI BISA MELALUI :*
+╠➥ *DANA/PULSA/OVO : 08127668234*
+╠➥ *PAYPAL : rashidsiregar28@gmail.com*
+╠➥ *TERIMA KASIH BANYAK YANG SUDAH MAU DONASI*
+║
+╚═〘 NEKO BOT 〙
+`
+}
+exports.sumbang = sumbang()
+function listChannel() {
+    return `Daftar channel: 
+1. ANTV
+2. GTV
+3. Indosiar
+4. iNewsTV
+5. KompasTV
+6. MNCTV
+7. METROTV
+8. NETTV
+9. RCTI
+10. SCTV
+11. RTV
+12. Trans7
+13. TransTV`
+}
+exports.listChannel = listChannel()
+function bahasalist() {
+    return `*List kode Bahasa*\n
+  *Code       Bahasa*
+    sq        Albanian
+    ar        Arabic
+    hy        Armenian
+    ca        Catalan
+    zh        Chinese
+    zh-cn     Chinese (China)
+    zh-tw     Chinese (Taiwan)
+    zh-yue    Chinese (Cantonese)
+    hr        Croatian
+    cs        Czech
+    da        Danish
+    nl        Dutch
+    en        English
+    en-au     English (Australia)
+    en-uk     English (United Kingdom)
+    en-us     English (United States)
+    eo        Esperanto
+    fi        Finnish
+    fr        French
+    de        German
+    el        Greek
+    ht        Haitian Creole
+    hi        Hindi
+    hu        Hungarian
+    is        Icelandic
+    id        Indonesian
+    it        Italian
+    ja        Japanese
+    ko        Korean
+    la        Latin
+    lv        Latvian
+    mk        Macedonian
+    no        Norwegian
+    pl        Polish
+    pt        Portuguese
+    pt-br     Portuguese (Brazil)
+    ro        Romanian
+    ru        Russian
+    sr        Serbian
+    sk        Slovak
+    es        Spanish
+    es-es     Spanish (Spain)
+    es-us     Spanish (United States)
+    sw        Swahili
+    sv        Swedish
+    ta        Tamil
+    th        Thai
+    tr        Turkish
+    vi        Vietnamese
+    cy        Welsh
+      `
+}
+exports.bahasalist = bahasalist()
